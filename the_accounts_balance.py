fba0b9c641037a1a39fa3f62069cb20f98a2ab02
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-16 13:14:46 +0200

    ajout d'une fonction pour la saisie de nombre par l'utilisateur

--- the_accounts_balance.py
+++ the_accounts_balance.py
@@ -18,9 +18,21 @@ def generate_balance_numbers():
     return list_of_balance_numbers
 
 
+def user_number_choice(list_of_balance_numbers):
+    while True:
+        user_input = input(f"Which number would you like to balance? choose 2 in {list_of_balance_numbers} ").split()
+        if user_input in list_of_balance_numbers:
+            list_of_balance_numbers.remove(user_input)
+            return user_input, list_of_balance_numbers
+        else:
+            print(f"Invalid choice. Please choose 2 in {list_of_balance_numbers}")
+
+
+
 def main():
     generate_target_number()
     list_balance_numbers = generate_balance_numbers()
+    user_numbers = user_number_choice(list_balance_numbers)
 
 
 if __name__=='__main__':
